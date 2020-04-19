0f09f2bbe66cba23f68f4fec81c06d8c610231fc
OscarBazanez <41228346+OscarBazanez@users.noreply.github.com>
2020-04-19 12:19:25 -0500

    Delete conexion.php

--- includes/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn=new mysqli("localhost", "aaa", "aaa", "mototli_prueba") or die("ConnectionFailed");
-
-$connect = new PDO("mysql:host=localhost;dbname=mototli_prueba", "aaa", "aaa");
-
-?>
-
